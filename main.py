f7f57fd72b0e1b8f6334cec67bceccbc86f830b5
Solucky2 <oneksiakprzemek@gmail.com>
2023-06-03 20:19:57 +0200

    zmiana uruchamiania programu

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from requests_betclic import *
 from selectors import *
 
 if __name__ == '__main__':
-    odds_two_possibilities_betclic(url=BetclicSelectorsTwo().betclic_tenis_url,
-                                   sheet_name='tenis')
-    odds_two_possibilities_betclic(url=BetclicSelectorsTwo().betclic_ksw_url, type_of_locator='div',
-                                   sheet_name='ksw')
-    odds_three_possibilities_betclic(url=BetclicSelectorsThree().betclic_serieA_url,
-                                     sheet_name='serieA')
+    for index, value in betclic_selectors_three.items():
+        odds_three_possibilities_betclic(url=value, sheet_name=index)
+    for index, value in betclic_selectors_two.items():
+        if index == 'ksw':
+            odds_two_possibilities_betclic(url=value, sheet_name=index,
+                                           type_of_locator='div')
+        else:
+            odds_two_possibilities_betclic(url=value, sheet_name=index)
--- selectors.py
+++ selectors.py
@@ -1,22 +1,11 @@
 
-class BetclicSelectorsTwo:
-    betclic_tenis_url = 'https://www.betclic.pl/tenis-s2'  # span
-    betclic_ksw_url = 'https://www.betclic.pl/sztuki-walki-s23/ksw-c4509'  # div
-
-
-class BetclicSelectorsThree:
-    betclic_serieA_url = 'https://www.betclic.pl/pilka-nozna-s1/serie-a-c6'  # div
-    betclic_ligue1_url = 'https://www.betclic.pl/pilka-nozna-s1/ligue-1-c4'  # div
-    betclic_la_liga_url = 'https://www.betclic.pl/pilka-nozna-s1/la-liga-c7'  # div
-
-
 betclic_selectors_two = {
-    'tenis_url': 'https://www.betclic.pl/tenis-s2',
-    'ksw_url': 'https://www.betclic.pl/sztuki-walki-s23/ksw-c4509'
+    'tenis': 'https://www.betclic.pl/tenis-s2',
+    'ksw': 'https://www.betclic.pl/sztuki-walki-s23/ksw-c4509'
 }
 
 betclic_selectors_three = {
-    'serieA_url': 'https://www.betclic.pl/pilka-nozna-s1/serie-a-c6',
-    'ligue1_URL': 'https://www.betclic.pl/pilka-nozna-s1/ligue-1-c4',
-    'la_liga_url': 'https://www.betclic.pl/pilka-nozna-s1/la-liga-c7'
+    'serieA': 'https://www.betclic.pl/pilka-nozna-s1/serie-a-c6',
+    'ligue1': 'https://www.betclic.pl/pilka-nozna-s1/ligue-1-c4',
+    'la_liga': 'https://www.betclic.pl/pilka-nozna-s1/la-liga-c7'
 }
